161e56e9dd527af8574ac231bb885e1d618d8230
132yse <1533540012@qq.com>
2018-09-17 09:22:54 +0800

    日常提交

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import Eplayer from './src/eplayer'
 let el = document.getElementById('ep')
 
 let ep = new Eplayer(el, {
-  src: 'https://new.jsyunbf.com/20180809/sT5p3w5X/index.m3u8',
+  src: 'https://api.dogecloud.com/player/get.m3u8?vcode=6b001ac5f0560207&userId=1&vtype=10&ext=.m3u8',
   themeColor: 'linear-gradient(to right,#0072ff ,#00e7ff)',
   hls:true
 })
