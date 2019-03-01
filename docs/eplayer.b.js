d16decb1d37b8bbace7ea8169253ba3be2974e29
132yse <1533540012@qq.com>
2019-03-01 10:12:51 +0800

    更新文档

--- docs/eplayer.b.js
+++ docs/eplayer.b.js
@@ -1,71 +1,60 @@
 class Eplayer extends HTMLElement {
   constructor() {
     super()
-    this.src = this.getAttribute("src")
-    this.type = this.getAttribute("type")
-    isMobile() ? this.minit() : this.init()
+    this.src = this.getAttribute('src')
+    this.type = this.src.includes('m3u8') ? 'mp4' : null
+    this.init()
     this.stream()
   }
 
   static get observedAttributes() {
-    return ["src", "type"]
+    return ['src']
   }
 
   attributeChangedCallback(name, _, newVal) {
-    if (name === "src") this.src = this.$(".source").src = newVal
-    if (name === "type") {
-      this.$(".source").type = `video/${newVal}`
-      this.type = newVal
-    }
+    if (name === 'src') this.src = this.$('.video').src = newVal
     this.stream()
     this.video.load()
   }
 
   waiting() {
-    this.$(".mark").classList.remove("playing")
-    this.$(".mark").classList.add("loading")
+    this.$('.mark').classList.remove('playing')
+    this.$('.mark').classList.add('loading')
   }
 
   stream() {
     switch (this.type) {
-      case "hls":
+      case 'hls':
         if (Hls.isSupported()) {
           let hls = new Hls()
           hls.loadSource(this.src)
           hls.attachMedia(this.video)
         }
         break
-      case "flv":
-        if (flvjs.isSupported()) {
-          let flvPlayer = flvjs.createPlayer({ type: "flv", url: this.src })
-          flvPlayer.attachMediaElement(this.video)
-          flvPlayer.load()
-        }
-        break
     }
   }
 
   canplay() {
-    this.$(".mark").classList.remove("loading")
-    this.$(".mark").classList.add("playing")
-    this.$(".playing").onclick = () => {
+    this.$('.mark').classList.remove('loading')
+    this.$('.mark').classList.add('playing')
+    this.$('.playing').onclick = () => {
       clearTimeout(this.timer)
       this.timer = setTimeout(() => {
         this.play()
       }, 200)
     }
-    this.$(".total").innerHTML = getTimeStr(this.video.duration)
+    this.$('.total').innerHTML = getTimeStr(this.video.duration)
   }
 
   play() {
     if (this.video.paused) {
       this.video.play()
-      this.$(".ep-video").style.display = "none"
-      this.$(".is-play").classList.replace("ep-play", "ep-pause")
+      this.$('.ep-video').style.display = 'none'
+      this.$('.is-play').classList.replace('ep-play', 'ep-pause')
     } else {
       this.video.pause()
-      this.$(".ep-video").style.display = "block"
-      this.$(".is-play").classList.replace("ep-pause", "ep-play")
+      this.$('.ep-video').style.display = 'block'
+      this.$('.is-play').classList.replace('ep-pause', 'ep-play')
     }
     return false
   }
@@ -73,12 +62,12 @@ class Eplayer extends HTMLElement {
   volume() {
     if (this.video.muted) {
       this.video.muted = false
-      setVolume(this.video.volume * 10, this.$(".line"))
-      this.$(".is-volume").classList.replace("ep-volume-off", "ep-volume")
+      setVolume(this.video.volume * 10, this.$('.line'))
+      this.$('.is-volume').classList.replace('ep-volume-off', 'ep-volume')
     } else {
       this.video.muted = true
-      setVolume(0, this.$(".line"))
-      this.$(".is-volume").classList.replace("ep-volume", "ep-volume-off")
+      setVolume(0, this.$('.line'))
+      this.$('.is-volume').classList.replace('ep-volume', 'ep-volume-off')
     }
     return false
   }
@@ -87,25 +76,24 @@ class Eplayer extends HTMLElement {
     let cTime = getTimeStr(this.video.currentTime)
     if (this.video.buffered.length) {
       let bufferEnd = this.video.buffered.end(this.video.buffered.length - 1)
-      this.$(".buffer").style.width =
-        (bufferEnd / this.video.duration) * this.$(".progress").clientWidth +
-        "px"
+      this.$('.buffer').style.width =
+        (bufferEnd / this.video.duration) * this.$('.progress').clientWidth +
+        'px'
     }
     let offset =
-      (this.video.currentTime / this.video.duration) * this.$(".bg").clientWidth
-    this.$(".now").innerHTML = cTime
-    this.$(".current").style.width = offset + "px"
-    this.$(".dot").style.left = offset + "px"
+      (this.video.currentTime / this.video.duration) * this.$('.bg').clientWidth
+    this.$('.now').innerHTML = cTime
+    this.$('.current').style.width = offset + 'px'
   }
 
   progress(e) {
-    let offset = e.offsetX / this.$(".progress").offsetWidth
+    let offset = e.offsetX / this.$('.progress').offsetWidth
     this.video.currentTime = this.video.duration * offset
     return false
   }
 
   down(e) {
-    this.disX = e.clientX - this.$(".dot").offsetLeft
+    this.disX = e.clientX - this.$('.cycle').offsetLeft
     document.onmousemove = e => this.move(e)
     document.onmouseup = () => {
       document.onmousemove = null
@@ -114,36 +102,44 @@ class Eplayer extends HTMLElement {
   }
 
   move(e) {
-    let offset = e.clientX - this.disX
+    let offset = e.clientX - this.disX + 7
     if (offset < 0) offset = 0
-    if (offset > this.$(".progress").clientWidth)
-      offset = this.$(".progress").clientWidth
-    this.$(".current").style.width = offset + "px"
-    this.$(".dot").style.left = offset + "px"
-    console.log(this.video.duration)
+    if (offset > this.$('.progress').clientWidth)
+      offset = this.$('.progress').clientWidth
+    this.$('.current').style.width = offset + 'px'
     this.video.currentTime =
-      (offset / this.$(".progress").clientWidth) * this.video.duration
-      console.log(this.video.currentTime)
+      (offset / this.$('.progress').clientWidth) * this.video.duration
     document.onmousemove = null
-    setTimeout(document.onmousemove = e => {
+    setTimeout(
+      (document.onmousemove = e => {
         if (e) this.move(e)
-    },30)
+      }),
+      30
+    )
   }
 
   alow() {
     clearTimeout(this.timer)
-    this.$(".controls").style.bottom = 0
-    this.$(".dot").style.bottom = 39 + "px"
-    this.$(".ep-video").style.bottom = 70 + "px"
+    this.$('.controls').style.bottom = 0
+    this.$('.ep-video').style.bottom = 70 + 'px'
     this.timer = setTimeout(() => {
       this.$(".controls").style.bottom = -42 + "px"
-      this.$(".dot").style.bottom = -42 + "px"
       this.$(".ep-video").style.bottom = 25 + "px"
     }, 5000)
   }
 
+  keyup(e) {
+    if (e && e.keyCode == 37) this.video.currentTime -= 10
+    if (e && e.keyCode == 39) this.video.currentTime += 10
+    if (e && e.keyCode == 38) this.video.volume += 0.05
+    if (e && e.keyCode == 40) this.video.volume -= 0.05
+    if (e && e.keyCode == 32) this.play()
+    let index = Math.floor(this.video.volume * 10)
+    setVolume(index, this.$('.line'))
+  }
+
   ended() {
-    this.$(".is-play").classList.replace("ep-pause", "ep-play")
+    this.$('.is-play').classList.replace('ep-pause', 'ep-play')
   }
 
   full() {
@@ -158,7 +154,7 @@ class Eplayer extends HTMLElement {
         document.msExitFullscreen()
       }
     } else {
-      let el = this.$(".eplayer")
+      let el = this.$('.eplayer')
       let rfs =
         el.requestFullScreen ||
         el.webkitRequestFullScreen ||
@@ -169,12 +165,12 @@ class Eplayer extends HTMLElement {
   }
 
   init() {
-    let link = document.createElement("link")
+    let link = document.createElement('link')
     link.setAttribute(
-      "href",
-      "https://at.alicdn.com/t/font_836948_6lbb2iu59.css"
+      'href',
+      'https://at.alicdn.com/t/font_836948_6lbb2iu59.css'
     )
-    link.setAttribute("rel", "stylesheet")
+    link.setAttribute('rel', 'stylesheet')
     document.head.appendChild(link)
     let html = `
       <style>
@@ -201,7 +197,6 @@ class Eplayer extends HTMLElement {
           bottom:0;
           padding:10px;
           background:linear-gradient(transparent,rgba(0,0,0,.5));
-          cursor: pointer; 
           transition: .3s ease-out;   
           z-index:1;   
         }
@@ -222,6 +217,7 @@ class Eplayer extends HTMLElement {
         .epicon{
           font-size:18px;
           transition: .3s;
+          cursor:pointer;
         }
         .epicon:hover{
           color:#fff;
@@ -264,8 +260,7 @@ class Eplayer extends HTMLElement {
         }
         .bg,.current,.buffer{
           left:0;
-          height:4px;
-          transform:scaleY(0.7);
+          height:3px;
           position:absolute;
           top:0;
         }
@@ -281,16 +276,28 @@ class Eplayer extends HTMLElement {
         }
         .dot{
           position:absolute;
-          left:0;
-          bottom:39px;
           border-radius: 50%;
           display: block;
           background:var(--theme,#00a1d6);
-          padding: 4px;
+          height: 9px;
+          width:9px;
+          right:-5px;
+          top:-3px;
           cursor:pointer;
-          transform:translateX(6px);
           z-index:1;
-          transition: .1s ease-out;
+        }
+        .cycle{
+          position:absolute;
+          border-radius: 50%;
+          display: block;
+          background:var(--theme,#00a1d6);
+          opacity:0.3;
+          height: 15px;
+          width:15px;
+          right:-8px;
+          top:-6px;
+          cursor:pointer;
+          z-index:1;
         }
         @keyframes loading{
           0%{
@@ -333,17 +340,16 @@ class Eplayer extends HTMLElement {
         }
       </style>
       <div class="eplayer">
-        <video id="video">
-          <source src="${this.src}" type="video/${
-      this.type ? this.type : "mp4"
-    }" class="source">
-        </video>
+        <video id="video" class="video" src="${this.src}"></video>
         <div class="mark loading"></div>
         <div class="controls" style="bottom:-45px">
           <div class="progress">
             <b class="bg"></b>
             <b class="buffer"></b>
-            <b class="current" style="width:0"></b>
+            <div class="current" style="width:0">
+              <div class="dot"></div>
+              <div class="cycle"></div>
+            </div>
           </div>
           <div class="options">
             <div class="left">
@@ -368,53 +374,43 @@ class Eplayer extends HTMLElement {
             </div>
           </div>
         </div>
-        <i class="dot" style="bottom:-45px"></i>
         <div class="epicon ep-video"></div>
       </div>
     `
-    let template = document.createElement("template")
+    let template = document.createElement('template')
     template.innerHTML = html
     this.attachShadow({
-      mode: "open"
+      mode: 'open'
     }).appendChild(template.content.cloneNode(true))
     this.dom()
   }
 
   dom() {
-    this.video = this.$("video")
+    window.onkeyup = e => this.keyup(e)
+    this.video = this.$('video')
     this.video.volume = 0.5
-    setVolume(this.video.volume * 10, this.$(".line"))
-    this.$(".is-volume").onclick = () => this.volume()
-    this.$(".line").forEach((item, index) => {
+    setVolume(this.video.volume * 10, this.$('.line'))
+    this.$('.is-volume').onclick = () => this.volume()
+    this.$('.line').forEach((item, index) => {
       item.onclick = () => {
         this.video.volume = index / 10
-        setVolume(index + 1, this.$(".line"))
+        setVolume(index + 1, this.$('.line'))
       }
     })
-    this.$(".dot").onmousedown = e => this.down(e)
-    this.$(".progress").onclick = e => this.progress(e)
+    this.$('.progress').onclick = e => this.progress(e)
     this.video.onwaiting = () => this.waiting()
     this.video.oncanplay = () => this.canplay()
     this.video.ontimeupdate = () => this.update()
+    this.$('.cycle').onmousedown = e => this.down(e)
 
     this.$(".eplayer").onmousemove = () => this.alow()
-    this.$(".ep-full").onclick = () => this.full()
-    this.$(".ep-video").onclick = this.$(".is-play").onclick = () => this.play()
+    this.$('.ep-full').onclick = () => this.full()
+    this.$('.ep-video').onclick = this.$('.is-play').onclick = () => this.play()
     this.video.onended = () => this.ended()
-    this.$(".mark").ondblclick = () => {
+    this.$('.mark').ondblclick = () => {
       clearTimeout(this.timer)
       this.full()
     }
-  }
-
-  minit() {
-    let html = `
-    <video id="video" controls controlslist="nodownload" style="width:100%">
-      <source src="${this.src}" type="video/${this.type ? this.type : "mp4"}">
-    </video>
-  `
-    this.innerHTML = html
-    this.video = document.getElementsByTagName("video")[0]
   }
 
   $(node) {
@@ -423,24 +419,24 @@ class Eplayer extends HTMLElement {
   }
 }
 
-customElements.define("e-player", Eplayer)
+customElements.define('e-player',Eplayer)
 
 function getTimeStr(time) {
   let h = Math.floor(time / 3600)
   let m = Math.floor((time % 3600) / 60)
   let s = Math.floor(time % 60)
-  h = h >= 10 ? h : "0" + h
-  m = m >= 10 ? m : "0" + m
-  s = s >= 10 ? s : "0" + s
-  return h === "00" ? m + ":" + s : h + ":" + m + ":" + s
+  h = h >= 10 ? h : '0' + h
+  m = m >= 10 ? m : '0' + m
+  s = s >= 10 ? s : '0' + s
+  return h === '00' ? m + ':' + s : h + ':' + m + ':' + s
 }
 
 function setVolume(index, node) {
   for (let j = index; j < node.length; j++) {
-    node[j].classList.remove("active")
+    node[j].classList.remove('active')
   }
   for (let i = 0; i < index; i++) {
-    node[i].classList.add("active")
+    node[i].classList.add('active')
   }
 }
 
@@ -450,8 +446,4 @@ function isFullScreen() {
     document.webkitIsFullScreen ||
     document.mozIsFullScreen
   )
-}
-
-function isMobile() {
-  return "ontouchend" in document.body
 }
